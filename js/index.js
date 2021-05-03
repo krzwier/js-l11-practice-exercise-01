07ab37071537d6ed703b23fbd68cac3f99bf9074
krzwier <krzwier@gmail.com>
2021-05-03 11:55:52 -0500

    Add api fetch and display users

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+
+const getData = async function () {
+    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+    const data = await usersRequest.json();
+    console.log(data);
+    const usersResults = data.results;
+    console.log(usersResults);
+    console.log(usersResults[0]);
+    displayUsers(usersResults);
+};
+
+const displayUsers = function (usersResults) {
+    randomFolks.innerHTML = "";
+    for (var user of usersResults) {
+        const country = user.location.country;
+        const name = user.name.first;
+        const imageUrl = user.picture.medium;
+        console.log(country, name, imageUrl);
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+            <h3>${name}</h3>
+            <p>${country}</p>
+            <img src=${imageUrl} alt="User avatar" />
+        `;
+        randomFolks.append(userDiv);
+    }
+};
+
+getData();
